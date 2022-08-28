6ff9c65038019299ce879df4c573c6ded329ce4c
gurjot74 <gagan.singh@tothenew.com>
2022-08-29 02:42:46 +0530

    minor bugs fixes

--- client/src/Components/Game.jsx
+++ client/src/Components/Game.jsx
@@ -1,33 +1,31 @@
-import { Component } from "react";
-import React from "react";
-import Modal from "react-modal";
-import { Link } from "react-router-dom";
-import { socket } from "../socket";
-import { toast } from "react-toastify";
-import { Navigate } from "react-router-dom";
-import zeroAudio from "../tictactoe1.mp3";
-import crossAudio from "../tictactoe2.mp3";
-import winningAudio from "../tictactoewin.mp3";
-import messageCleaner from "../utils/messageCleaner";
-import jwt from "jwt-decode";
+import { Component } from 'react';
+import React from 'react';
+import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
+import { socket } from '../socket';
+import { toast } from 'react-toastify';
+import { Navigate } from 'react-router-dom';
+import zeroAudio from '../tictactoe1.mp3';
+import crossAudio from '../tictactoe2.mp3';
+import winningAudio from '../tictactoewin.mp3';
+import messageCleaner from '../utils/messageCleaner';
+import jwt from 'jwt-decode';
 
-const user_id =
-  localStorage.getItem("token") && jwt(localStorage.getItem("token"))._id;
 let subtitle;
 const customStyles = {
   content: {
-    top: "20%",
-    left: "49%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
+    top: '20%',
+    left: '49%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
   },
 };
 const audio1 = new Audio(zeroAudio);
 const audio2 = new Audio(crossAudio);
 const audio3 = new Audio(winningAudio);
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 class Game extends Component {
   constructor() {
@@ -38,16 +36,20 @@ class Game extends Component {
     this.turndesign = 0;
     this.Interval = null;
   }
+
+  user_id =
+  localStorage.getItem('token') && jwt(localStorage.getItem('token'))._id;
+
   emptyBlocks = {
-    block1: "",
-    block2: "",
-    block3: "",
-    block4: "",
-    block5: "",
-    block6: "",
-    block7: "",
-    block8: "",
-    block9: "",
+    block1: '',
+    block2: '',
+    block3: '',
+    block4: '',
+    block5: '',
+    block6: '',
+    block7: '',
+    block8: '',
+    block9: '',
   };
   state = {
     zeroOrCross: true,
@@ -63,7 +65,7 @@ class Game extends Component {
   };
 
   afterOpenModal = () => {
-    subtitle.style.color = "#f00";
+    subtitle.style.color = '#f00';
   };
 
   closeModal = () => {
@@ -75,26 +77,26 @@ class Game extends Component {
   };
 
   handleClick = (e) => {
-    if (this.turn === user_id && e.target.innerHTML === "") {
-      this.value = (this.state.zeroOrCross && "O") || "X";
+    if (this.turn === this.user_id && e.target.innerHTML === '') {
+      this.value = (this.state.zeroOrCross && 'O') || 'X';
       this.setState({
         blocks: { ...this.state.blocks, [e.target.classList[1]]: this.value },
       });
       socket.emit(
-        "opponentTurnPayload",
+        'opponentTurnPayload',
         {
           zeroOrCross: !this.state.zeroOrCross,
           blocks: { ...this.state.blocks, [e.target.classList[1]]: this.value },
           value: this.value,
         },
-        user_id
+        this.user_id
       );
       this.turn = false;
     }
   };
   hasNullBlocks(target) {
     for (var member in target) {
-      if (target[member] === "") return true;
+      if (target[member] === '') return true;
     }
     return false;
   }
@@ -108,20 +110,20 @@ class Game extends Component {
       setTimeout(
         (i) => {
           if (i % 2 === 0) {
-            block1.innerHTML = "Tic";
-            block1.style.color = "Orange";
-            block2.innerHTML = "Tac";
-            block2.style.color = "Orange";
-            block3.innerHTML = "Toe";
-            block3.style.color = "Orange";
+            block1.innerHTML = 'Tic';
+            block1.style.color = 'Orange';
+            block2.innerHTML = 'Tac';
+            block2.style.color = 'Orange';
+            block3.innerHTML = 'Toe';
+            block3.style.color = 'Orange';
           }
           if (i % 2 !== 0) {
             block1.innerHTML = winner;
-            block1.style.color = "red";
+            block1.style.color = 'red';
             block2.innerHTML = winner;
-            block2.style.color = "red";
+            block2.style.color = 'red';
             block3.innerHTML = winner;
-            block3.style.color = "red";
+            block3.style.color = 'red';
           }
         },
         i * 100,
@@ -150,9 +152,9 @@ class Game extends Component {
       ) {
         this.winningDesign(a, b, c);
         this.openModal();
-        document.getElementsByClassName("turn-selector")[0].style.display =
-          "none";
-        socket.emit("leaveRoom", user_id);
+        document.getElementsByClassName('turn-selector')[0].style.display =
+          'none';
+        socket.emit('leaveRoom', this.user_id);
         audio3.play();
         return;
       }
@@ -160,17 +162,17 @@ class Game extends Component {
     if (!this.hasNullBlocks(this.state.blocks)) {
       this.value = false;
       this.openModal();
-      document.getElementsByClassName("turn-selector")[0].style.display =
-        "none";
-      socket.emit("leaveRoom", user_id);
+      document.getElementsByClassName('turn-selector')[0].style.display =
+        'none';
+      socket.emit('leaveRoom', this.user_id);
       audio3.play();
     }
-    this.value === "X" && audio2.play();
-    this.value === "O" && audio1.play();
+    this.value === 'X' && audio2.play();
+    this.value === 'O' && audio1.play();
   };
 
   componentDidMount() {
-    socket.on("refreshPage", (message) => {
+    socket.on('refreshPage', (message) => {
       toast.error(message);
       setTimeout(() => {
         window.location.reload();
@@ -178,64 +180,64 @@ class Game extends Component {
     });
     this.Interval = setInterval(() => {
       if (this.turndesign % 3 === 0)
-        document.getElementsByClassName("turn-design")[0].innerHTML = ".";
+        document.getElementsByClassName('turn-design')[0].innerHTML = '.';
       else if (this.turndesign % 3 === 1)
-        document.getElementsByClassName("turn-design")[0].innerHTML = "..";
+        document.getElementsByClassName('turn-design')[0].innerHTML = '..';
       else if (this.turndesign % 3 === 2)
-        document.getElementsByClassName("turn-design")[0].innerHTML = "...";
+        document.getElementsByClassName('turn-design')[0].innerHTML = '...';
       this.turndesign++;
       if (this.turndesign > 3) this.turndesign = 0;
     }, 1000);
 
-    socket.on("connect", () => {
-      console.log("connect");
+    socket.on('connect', () => {
+      console.log('connect');
       socket.emit(
-        "gameComponentRefresh",
-        localStorage.getItem("token"),
+        'gameComponentRefresh',
+        localStorage.getItem('token'),
         socket.emit
       );
 
-      socket.on("afterGameComponentRefresh", (message) => {
-        if (messageCleaner(message) === "toHome") {
+      socket.on('afterGameComponentRefresh', (message) => {
+        if (messageCleaner(message) === 'toHome') {
           this.setState({ ...this.state, redirectToHome: true });
-        } else if (messageCleaner(message) === "toLogin") {
+        } else if (messageCleaner(message) === 'toLogin') {
           this.setState({ ...this.state, redirectToLogin: true });
-          toast.error("User Not logged in");
+          toast.error('User Not logged in');
         } else {
-          socket.emit("onGameComponentMount"); //already writen below same event of socket.emit(ongamecomponentmount) and it also runs on componentdidmount but already socket from backend is in
+          socket.emit('onGameComponentMount'); //already writen below same event of socket.emit(ongamecomponentmount) and it also runs on componentdidmount but already socket from backend is in
         } // progress due to onGameComponentrefresh so server not catch gamecomponentmount event and when it finishes the work ,we know gamecomponentmount not fire again,so to fire after
       }); //gamecomponentrefresh finishes its work we can wrap gamecomponentmount event in setTimeout and it works also  but it is not a better way for production levels thats why written in
     }); //else and below written socket.ongamecomponentmount not works as already explained so have to additionally write it here
 
-    socket.emit("onGameComponentMount"); // socket.onconnect events fires only on refresh ,so on room joining from home and redirecting to game we have to fire another socket.ongamecomponentmount as socket.onconnect not fires in this situation
-    socket.on("message", (gamedata, turn) => {
+    socket.emit('onGameComponentMount'); // socket.onconnect events fires only on refresh ,so on room joining from home and redirecting to game we have to fire another socket.ongamecomponentmount as socket.onconnect not fires in this situation
+    socket.on('message', (gamedata, turn) => {
       this.setState(gamedata);
       this.value = gamedata.value;
       this.turn = turn;
     });
 
-    socket.on("opponentjoined", (username) => {
-      toast.success(username + "joined", {
+    socket.on('opponentjoined', (username) => {
+      toast.success(username + 'joined', {
         toastId: 1234,
       });
     });
-    socket.on("inWaiting", () => {
-      document.getElementsByClassName("parentloader")[0].style.display = "none";
+    socket.on('inWaiting', () => {
+      document.getElementsByClassName('parentloader')[0].style.display = 'none';
       this.setState({ ...this.state, startGameModal: true });
     });
 
-    socket.on("startGame", (gamedata, turn) => {
+    socket.on('startGame', (gamedata, turn) => {
       this.turn = turn;
-      document.getElementsByClassName("parentloader")[0].style.display = "none";
-      document.getElementsByClassName("turn-selector")[0].style.display =
-        "block";
+      document.getElementsByClassName('parentloader')[0].style.display = 'none';
+      document.getElementsByClassName('turn-selector')[0].style.display =
+        'block';
       if (gamedata)
         return this.setState({ ...gamedata, startGameModal: false });
       this.setStartGame();
     });
 
-    socket.on("anotherplayerdisconnected", () =>
-      console.log("anotherplayerdisconnected")
+    socket.on('anotherplayerdisconnected', () =>
+      console.log('anotherplayerdisconnected')
     );
   }
 
@@ -253,25 +255,25 @@ class Game extends Component {
     console.log(this.state.blocks);
     return (
       <>
-        {this.state.redirectToHome && <Navigate to="/home" replace={true} />}
-        {this.state.redirectToLogin && <Navigate to="/" replace={true} />}
+        {this.state.redirectToHome && <Navigate to='/home' replace={true} />}
+        {this.state.redirectToLogin && <Navigate to='/' replace={true} />}
         <Modal
           isOpen={this.state.modalIsOpen}
           onRequestClose={this.closeModal}
           style={customStyles}
-          contentLabel="Example Modal"
+          contentLabel='Example Modal'
           shouldCloseOnOverlayClick={false}
         >
-          <h5 style={{ textAlign: "center" }}>RESULT..!!</h5>
+          <h5 style={{ textAlign: 'center' }}>RESULT..!!</h5>
           {(this.value && (
-            <p style={{ textAlign: "center" }}>
-              {(this.turn === user_id && "You Lost") || "You Win"}
+            <p style={{ textAlign: 'center' }}>
+              {(this.turn === this.user_id && 'You Lost') || 'You Win'}
             </p>
-          )) || <p style={{ textAlign: "center" }}>Tie</p>}
-          <Link to="/home">
+          )) || <p style={{ textAlign: 'center' }}>Tie</p>}
+          <Link to='/home'>
             <h6>
-              Start New Game{" "}
-              <i className="fa fa-refresh" aria-hidden="true"></i>
+              Start New Game{' '}
+              <i className='fa fa-refresh' aria-hidden='true'></i>
             </h6>
           </Link>
         </Modal>
@@ -279,56 +281,56 @@ class Game extends Component {
           isOpen={this.state.startGameModal}
           onRequestClose={this.setStartGame}
           style={customStyles}
-          contentLabel="Example Modal"
+          contentLabel='Example Modal'
           shouldCloseOnOverlayClick={false}
         >
           <h5>Waiting for another player to join..</h5>
         </Modal>
-        <h1 className="heading">Tic Tac Toe</h1>
-        <div className="container position-relative">
-          <div className="parentloader">
-            <div className="spinner-border spinner"></div>
+        <h1 className='heading'>Tic Tac Toe</h1>
+        <div className='container position-relative'>
+          <div className='parentloader'>
+            <div className='spinner-border spinner'></div>
           </div>
-          <div className="child-container">
-            <div className="turn-selector">
-              {(this.turn === user_id && "Your turn") ||
+          <div className='child-container'>
+            <div className='turn-selector'>
+              {(this.turn === this.user_id && 'Your turn') ||
                 "Waiting For Opponent's Turn"}
-              <span className="turn-design"></span>
+              <span className='turn-design'></span>
             </div>
-            <div className="row">
-              <span className="block block1" onClick={this.handleClick}>
+            <div className='row'>
+              <span className='block block1' onClick={this.handleClick}>
                 {this.state.blocks.block1}
               </span>
-              <span className="block block2" onClick={this.handleClick}>
+              <span className='block block2' onClick={this.handleClick}>
                 {this.state.blocks.block2}
               </span>
-              <span className="block block3" onClick={this.handleClick}>
+              <span className='block block3' onClick={this.handleClick}>
                 {this.state.blocks.block3}
               </span>
             </div>
-            <div className="row">
+            <div className='row'>
               <span
-                className="block block4"
+                className='block block4'
                 ref={this.blockRef}
                 onClick={this.handleClick}
               >
                 {this.state.blocks.block4}
               </span>
-              <span className="block block5" onClick={this.handleClick}>
+              <span className='block block5' onClick={this.handleClick}>
                 {this.state.blocks.block5}
               </span>
-              <span className="block block6" onClick={this.handleClick}>
+              <span className='block block6' onClick={this.handleClick}>
                 {this.state.blocks.block6}
               </span>
             </div>
-            <div className="row">
-              <span className="block block7" onClick={this.handleClick}>
+            <div className='row'>
+              <span className='block block7' onClick={this.handleClick}>
                 {this.state.blocks.block7}
               </span>
-              <span className="block block8" onClick={this.handleClick}>
+              <span className='block block8' onClick={this.handleClick}>
                 {this.state.blocks.block8}
               </span>
-              <span className="block block9" onClick={this.handleClick}>
+              <span className='block block9' onClick={this.handleClick}>
                 {this.state.blocks.block9}
               </span>
             </div>
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const mongoose = require('mongoose');
-const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const { instrument } = require("@socket.io/admin-ui");
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { instrument } = require('@socket.io/admin-ui');
 const app = express();
-const cors = require("cors");
-const setheader = require('./utils/setheader')
+const cors = require('cors');
+const setheader = require('./utils/setheader');
 const httpServer = createServer(app);
-const joinRoom=require('./routes/joinroom')
-const userAuth=require('./routes/auth')
-const home=require('./routes/home')
-const getFriendsInWaiting = require('./routes/getFriendsInWaiting')
+const joinRoom = require('./routes/joinroom');
+const userAuth = require('./routes/auth');
+const home = require('./routes/home');
+const getFriendsInWaiting = require('./routes/getFriendsInWaiting');
 const Gamedata = require('./models/gamedata');
-const onConnectionRefresh = require('./controllers/connectionRefreshController')
+const onConnectionRefresh = require('./controllers/connectionRefreshController');
 const io = new Server(httpServer, {
   cors: {
-  origin: "*",
- credentials:true
-  
+    origin: '*',
+    credentials: true,
   },
 });
 require('./controllers/polling')(io);
 
-mongoose.connect('mongodb+srv://dbuser:Waheguru747477%40@cluster0.pkxnk.mongodb.net/tictactoe?retryWrites=true&w=majority')
+mongoose
+  .connect(
+    'mongodb+srv://dbuser:Waheguru747477%40@cluster0.pkxnk.mongodb.net/tictactoe?retryWrites=true&w=majority'
+  )
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error(`${err}`));
 
@@ -34,93 +36,112 @@ app.use(express.json());
 app.use(setheader);
 app.use(
   cors({
-    origin: "*",
-    credentials: "true",
-  }));
+    origin: '*',
+    credentials: 'true',
+  })
+);
 
- app.use('/api',home);
- app.use('/api',userAuth);
- app.use('/api',joinRoom);
- app.use('/api',getFriendsInWaiting);
- 
-    io.on("connection", (socket) => {
-    console.log(socket.id);
-    socket.on('gameComponentRefresh',(jwtoken,obj)=>{
-    onConnectionRefresh(jwtoken,obj).then((message)=>{
-    socket.emit('afterGameComponentRefresh',message.toString());
-    }).catch((err)=>{
-    socket.emit('afterGameComponentRefresh',err.toString());
-    })})
-     socket.on("joinRoom", (roomId,username) => {
-    console.log("joinRoom", roomId);
+app.use('/api', home);
+app.use('/api', userAuth);
+app.use('/api', joinRoom);
+app.use('/api', getFriendsInWaiting);
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  socket.on('gameComponentRefresh', (jwtoken, obj) => {
+    onConnectionRefresh(jwtoken, obj)
+      .then((message) => {
+        socket.emit('afterGameComponentRefresh', message.toString());
+      })
+      .catch((err) => {
+        socket.emit('afterGameComponentRefresh', err.toString());
+      });
+  });
+  socket.on('joinRoom', (roomId, username) => {
+    console.log('joinRoom', roomId);
     const room = roomId.toString();
-    
-      socket.emit("roomAvailable");
-      socket.join(room);
-    
-      socket.on("leaveRoom", async (user_id) => {
-      try{ const data = await Gamedata.findOneAndUpdate({roomId:room},{$pull:{users:user_id}},{ new:true});
-      data && data.users.length === 0 && await Gamedata.deleteOne({roomId:room});
-      socket.leave(room);
-      }catch(err)
-      {
-       console.log(err)
+
+    socket.emit('roomAvailable');
+    socket.join(room);
+
+    socket.on('leaveRoom', async (user_id) => {
+      try {
+        const data = await Gamedata.findOneAndUpdate(
+          { roomId: room },
+          { $pull: { users: user_id } },
+          { new: true }
+        );
+        data &&
+          data.users.length === 0 &&
+          (await Gamedata.deleteOne({ roomId: room }));
+        socket.leave(room);
+        socket.removeAllListeners('onGameComponentMount');
+        socket.removeAllListeners('opponentTurnPayload');
+        socket.removeAllListeners('disconnect');
+        socket.removeAllListeners('leaveRoom');
+      } catch (err) {
+        console.log(err);
       }
     });
-      socket.on("opponentTurnPayload", async (gamedata,user_id) => {
-      try{
-      const data=await Gamedata.findOne({roomId:room});
-      if(data){
-      data.gamedata=gamedata;
-      data.turn=data.users.filter((user)=>user.toString()!==user_id)[0].toString();
-      await data.save();
-      socket.to(room).emit("message", gamedata,data.turn)}}
-      catch(err){
-      console.log(err);
-      }});
-      socket.on("onGameComponentMount", async () => {
+    socket.on('opponentTurnPayload', async (gamedata, user_id) => {
+      try {
+        const data = await Gamedata.findOne({ roomId: room });
+        if (data) {
+          data.gamedata = gamedata;
+          data.turn = data.users
+            .filter((user) => user.toString() !== user_id)[0]
+            .toString();
+          await data.save();
+          socket.to(room).emit('message', gamedata, data.turn);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+    socket.on('onGameComponentMount', async () => {
       console.log('gamecomponentmount');
       io.sockets.adapter.rooms.get(room) && //room is accessed as js follows closure such that the instance of socket in which join room is fired ,when that same instance of socket emits gamecomponentmount the room is accessed from join room as it is protected because of closure
-      io.sockets.adapter.rooms.get(room).size === 1 &&
-      socket.emit("inWaiting");
-      if( io.sockets.adapter.rooms.get(room) && io.sockets.adapter.rooms.get(room).size >= 2)
-      { 
-        const data = await Gamedata.findOne({roomId:room});
-        if(data.turn === ''){
-        data.turn = data.users[0];
-        await data.save();
+        io.sockets.adapter.rooms.get(room).size === 1 &&
+        socket.emit('inWaiting');
+      if (
+        io.sockets.adapter.rooms.get(room) &&
+        io.sockets.adapter.rooms.get(room).size >= 2
+      ) {
+        const data = await Gamedata.findOne({ roomId: room });
+        if (data.turn === '') {
+          data.turn = data.users[0];
+          await data.save();
         }
-        if(data.users.length === 1)
-        return socket.emit('inWaiting')
-        io.in(room).emit("startGame",data.gamedata,data.turn);
-         socket.to(room).emit('opponentjoined',username)}
+        if (data.users.length === 1) return socket.emit('inWaiting');
+        io.in(room).emit('startGame', data.gamedata, data.turn);
+        socket.to(room).emit('opponentjoined', username);
+      }
     });
-      socket.on("disconnect", () => {
-      socket.to(room).emit("anotherplayerdisconnected");
-    })
+    socket.on('disconnect', () => {
+      socket.to(room).emit('anotherplayerdisconnected');
+    });
   });
-  socket.on('quitRoom',async (roomId)=>{
-const result = await Gamedata.deleteOne({roomId});
-if(result.acknowledged)
-socket.to(roomId).emit('refreshPage',"Opponent left");
-socket.emit('refreshPage')
-  })
+  socket.on('quitRoom', async (roomId) => {
+    const result = await Gamedata.deleteOne({ roomId });
+    if (result.acknowledged)
+      socket.to(roomId).emit('refreshPage', 'Opponent left');
+    socket.emit('refreshPage');
+  });
 });
 httpServer.listen(process.env.PORT);
 process
-  .on("unhandledRejection", (reason, p) => {
-    console.error(reason, "Unhandled Rejection at Promise", p);
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
   })
-  .on("uncaughtException", (err) => {
-    console.error(err, "Uncaught Exception thrown");
+  .on('uncaughtException', (err) => {
+    console.error(err, 'Uncaught Exception thrown');
     process.exit(1);
   });
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
