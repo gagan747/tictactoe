a95838f48307cfbf66400e1a4633d36c249faaf2
gurjot74 <gagan.singh@tothenew.com>
2022-12-25 01:45:12 +0530

    builded

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-const hostname = process.env.REACT_APP_STAGE === 'development' ? 'http://localhost:5000' :'https://tictactoe-ghj6.onrender.com'
+const hostname = process.env.REACT_APP_STAGE === 'development' ? 'http://localhost:5000' :'https://tictactoe-zj8e.onrender.com'
 export default hostname;
