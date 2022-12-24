0fdf9142670fc221c29e92ff10c8c63429b7917f
gurjot74 <gagan.singh@tothenew.com>
2022-12-25 01:19:14 +0530

    changes

--- index.js
+++ index.js
@@ -149,10 +149,10 @@ process
     process.exit(1);
   });
 
-if (process.env.NODE_ENV === 'production') {
+
   app.use(express.static('client/build'));
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+
