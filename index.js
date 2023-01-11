106e3b0434a73ffaf072fe238a136491877ec79e
gurjot74 <gagan.singh@tothenew.com>
2023-01-11 17:10:21 +0530

    latest commit

--- index.js
+++ index.js
@@ -148,6 +148,6 @@ process
     console.error(err, 'Uncaught Exception thrown');
     process.exit(1);
   });
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
